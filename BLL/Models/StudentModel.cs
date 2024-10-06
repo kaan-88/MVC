813d940f818fafd6c61f2fc7e107548a6abd58ba
kaan-88 <158096269+kaan-88@users.noreply.github.com>
2024-10-06 13:25:47 +0300

    Add files via upload

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using BLL.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; set; }
+
+        public string Name => Record.Name;
+        public string Surname => Record.Surname;
+        public DateTime BirthDate => (DateTime)Record.BirthDate;
+        public decimal GPA => (decimal)Record.Gpa;
+
+        public string FullName => $"{Record.Name} {Record.Surname}";
+
+    }
+}
